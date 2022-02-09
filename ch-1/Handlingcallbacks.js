9ca33e42c150c46476adc46ad412b3492a8927cd
Umang273481 <99251252+Umang273481@users.noreply.github.com>
2022-02-09 13:34:44 +0530

    All about handling call backs

--- ch-1/Handlingcallbacks.js
+++ ch-1/Handlingcallbacks.js
@@ -1,6 +1,6 @@
 var myAsyncFunction = function(arg1, arg2, callback) {
     console.log(arg1,arg2);
-    setTimeout(function() {
+    setTimeout(function() { 
     // 1 second later, we are done with the I/O, call the callback
         callback();
     }, 1000)
