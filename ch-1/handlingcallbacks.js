c287fa1aae03c260fd85d1df77b1227fc9c552b1
shreshc <shresh.chaudhari@eronkan.com>
2022-02-09 16:58:58 +0530

    nodejs first examle

---
+++ ch-1/handlingcallbacks.js
@@ -0,0 +1,10 @@
+var myAsyncFunction = function(arg1, arg2, callback) {
+    console.log(arg1,arg2);
+    setTimeout(function() {
+    // 1 second later, we are done with the I/O, call the callback
+        callback();
+    }, 1000)
+}
+const arg1=200;
+const arg2=300;
+myAsyncFunction(arg1,arg2);
---
+++ ch-2 and ch-3/schedulingcallbacks1.js
@@ -0,0 +1,11 @@
+//waiting for infinte time 
+var open = false;
+
+setTimeout(function() {
+    open = true;
+}, 1000)
+
+while(!open) {
+    // wait
+}
+console.log('opened!');
