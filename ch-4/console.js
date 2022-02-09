51269030056256da94b3ee33b52436bfbe485e3c
shreshc <shresh.chaudhari@eronkan.com>
2022-02-09 18:35:08 +0530

    all program about console

---
+++ ch-4/console.js
@@ -0,0 +1,14 @@
+//example-1::
+
+console.log("Hello World!");
+
+//example-2::
+var a = {1:true, 2:false};
+console.log(a);
+
+//example-3::
+var a = {1: true, 2: false};
+console.log('This is a number: %d, and this is a string: %s, and this is an object outputted as JSON: %j', 42, 'Hello', a);
+
+console.warn("Warning!")
+console.trace();
