b5bddb293fa2d471d4f0a90344feeab742c11a23
shreshc <shresh.chaudhari@eronkan.com>
2022-02-17 13:21:41 +0530

    About TCP Server

---
+++ ch-11/TCPserver.js
@@ -0,0 +1,28 @@
+require('net').createServer(function(socket) {
+        // new connection
+        socket.on('data', function(data) {
+            // got data
+            console.log("data = "+data.toString());
+        });
+        socket.on('end', function(data) {
+            // connection closed
+            console.log("Connection closed");
+        });
+        socket.write('Some string');
+        
+        var port = 8080;
+        var host = '127.0.0.1';
+        var server = require('http').createServer();
+        server.listen(port,host, function() {
+            console.log('server listening on port ' + port);
+        });
+
+        socket.setTimeout(60000, function() {
+            socket.end('idle timeout, disconnecting, bye!');
+        });
+
+        //server.close();
+        //server.on('close', function() {
+        //    console.log('server closed!');
+        //})
+    }).listen(8081);
