6ace871fa1503024ed8f302a92a3be699fd40576
shreshc <shresh.chaudhari@eronkan.com>
2022-02-24 11:06:13 +0530

    client

---
+++ ch-14/client.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var conn = require('net').createConnection('./Example15.sock');
+conn.on('fd', function(fileDesc) {
+    fs.write(fileDesc, "this is the child!\n", function() {
+        conn.end();
+    });
+});
