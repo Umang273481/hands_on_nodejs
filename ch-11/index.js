813efef093beb55434c35197c309fb704014f348
shreshc <shresh.chaudhari@eronkan.com>
2022-02-24 10:50:59 +0530

    Chapter-11:TCP

--- ch-11/index.js
+++ ch-11/index.js
@@ -1,5 +1,9 @@
-var ex1 = require('./Ex-1');//for server
-var ex2 = require('./Ex-2');//for client
+const client = require('./TCPclient1.js');
+const server = require('./TCPserver1.js');
+var ex1 = require('./Ex-1.js');
+var ex2 = require('./Ex-2.js');
 
-ex1();
-ex2();
+//ex1();
+//ex2();
+//client();
+//server();
